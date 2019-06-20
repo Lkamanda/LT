94416344f82c585abc6f5d63a109a5cc10a81e5d
zhoujialin <1599200510@qq.com>
2019-06-20 20:28:55 +0800

    connect mysql and select data

--- connect_mysql/connect.py
+++ connect_mysql/connect.py
@@ -1,20 +1,57 @@
 import pymysql
+from collections import OrderedDict
+from connect_mysql.time import get_start_time, get_end_time
 
-try:
-    db=pymysql.connect(
-        host='cn-master.cgl0jlkt5nez.rds.cn-north-1.amazonaws.com.cn',
-        user='jialin',
-        password='jialin',
-        db='stats',
-        port=3306
-                        )
-    coursor = db.cursor()
-    sql = 'select * from t_record_start limit 10'
-    a = coursor.execute(sql)
-    print(a)
-    for r in a:
-        print(r.total)
-    db.close()
+D = OrderedDict()
+select_list = ["360", "Baidu", "HUAWEI", "Mi", "OPPO", "Tencent", "VIVO", "WDJ", "App Store"]
 
-except Exception as e:
-    print(e)
+
+def get_D():
+    global all_number
+    all_number = 0
+    try:
+        db = pymysql.connect(
+            host='cn-master.cgl0jlkt5nez.rds.cn-north-1.amazonaws.com.cn',
+            user='jialin',
+            password='jialin',
+            db='stats',
+            port=3306
+                            )
+        coursor = db.cursor()
+        for i in select_list:
+            global sql
+            print(i)
+            if i == "App Store":
+                sql = 'select * from t_record_start where rstart_appChannel="{0}" and rstart_type="1"' \
+                      ' and rstart_platform="2" and rstart_clientCreateDate between "{1}" and ' \
+                      '"{2}"'.format(i, start_time, end_time)
+            else:
+                sql = 'select * from t_record_start where rstart_appChannel="{0}" and rstart_type="1"' \
+                      ' and rstart_platform="1" and rstart_clientCreateDate between "{1}" and ' \
+                      '"{2}"'.format(i, start_time, end_time)
+            a = coursor.execute(sql)
+            # a 为查询到数据数量
+
+            D[i] = a
+            print(a)
+            # 打印出每一条查询结果
+            for y in range(a):
+                result = coursor.fetchone()
+                print(result)
+            all_number = all_number + D[i]
+        D["all_number"] = all_number
+        db.close()
+        print(D)
+        # for key in D:
+        #     print(key, D[key])
+        return D
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    x1, y1, z1, x2, y2, z2 = eval(input("start_time 和 end_time 格式如（2019,6,19,2019,6,20）："))
+    start_time = get_start_time(x1, y1, z1)
+    end_time = get_end_time(x2, y2, z2)
+    get_D()
---
+++ connect_mysql/time.py
@@ -0,0 +1,63 @@
+import time
+import datetime
+# from datetime import datetime, timedelta
+#
+# print( datetime.today().date())
+# day =  datetime.today().date()
+# ts = int(time.mktime(time.strptime(str(day), '%Y-%m-%d')))
+# print(ts)
+
+#
+# def get_start_time():
+#     today = datetime.date.today()
+#     tomorrow = today + datetime.timedelta(days=-1)
+#     print(tomorrow)
+#     dt = str(tomorrow) + ' ' + '00:00:00'
+#     ts = int(time.mktime(time.strptime(dt, "%Y-%m-%d %H:%M:%S"))) * 1000
+#     print(ts)
+#     return ts
+#
+#
+# def get_end_time():
+#     dt = time.strftime('%Y-%m-%d') + ' ' + '00:00:00'
+#     print(dt)
+#     #
+#     # dt = '2019-06-20 00:00:00'
+#     ts = int(time.mktime(time.strptime(dt, "%Y-%m-%d %H:%M:%S"))) * 1000
+#     print(ts)
+#     return ts
+
+
+def get_start_time(x1, y1, z1):
+    # s = datetime.datetime(2019, 06, 20, 00, 00, 00)
+    s = datetime.datetime(x1, y1, z1, 00, 00, 00)
+    print(s)
+    start_time = int(time.mktime(s.timetuple())) * 1000
+    print(start_time)
+    return start_time
+
+
+def get_end_time(x2, y2, z2):
+    s = datetime.datetime(x2, y2, z2, 00, 00, 00)
+    print(s)
+    end_time = int(time.mktime(s.timetuple())) * 1000
+    print(end_time)
+    return end_time
+
+
+# if __name__ == '__main__':
+
+    # get_start_time(x1=2019, y1=6, z1=19)
+    # # get_end_time()
+
+    # x1, y1, z1, x2, y2, z2 = input()
+    # a,b =input("")
+    # print(a,b)
+
+    # n, m, c = eval(input("4th enter:"))
+    # print(n,m,c)
+    #
+    # x1, y1, z1, x2, y2, z2 = eval(input("start_time 和 end_time 格式如（·）："))
+
+
+
